e94494bc8856e15676e737da250aecd879a4ee3a
Akash <akash.fbu@gmail.com>
2026-08-30 20:44:14 +0600

    done

--- my-app/src/components/footer.tsx
+++
@@ -1,5 +0,0 @@
-// import type { ReactNode } from "react";
-
-export default function Footer() {
-  return <footer>Footer</footer>;
-}
--- my-app/src/page/Service.tsx
+++
@@ -1,8 +0,0 @@
-export default function Service() {
-    return (
-        <div>
-            <h1>Service</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-        </div>
-    )
-}
